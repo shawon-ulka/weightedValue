ec84fb7efc9d37bc43aaf5ae67fc70db82660962
shawon-ulka <md.shawon.islam@ulkasemi.com>
2023-05-22 18:09:59 +0600

    added all corners and change

---
+++ sakifwv.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+readFile='eng_vs_2.0.xlsx'
+writeFile="Ritu_Apu_Eng_V2.0.xlsx"
+
+def checktargetInv(dct,valType):
+    if "inv"in dct['IP_Name'].lower():
+        return dct[valType]*.4
+    else:
+        return dct[valType]*.1
+
+df=pd.read_excel(readFile,header=2)
+# comparison =df[1:]
+filtered=df[["IP_Name","Tech","VT","Track","Parameter","PexCorner" ,"PexTemp","Unit","TargetValue","ReferenceValue"]]
+filtered=filtered.sort_values(by=['Parameter',"PexTemp","PexCorner","VT"])
+delay=filtered[(filtered["Parameter"]=="stage_delay")]
+leakage=filtered[(filtered["Parameter"]=="iddq_stage")]
+delay["wieghted_Target"]=delay.apply(lambda x :checktargetInv(x,"TargetValue"),axis=1)
+delay["wieghted_Reference"]=delay.apply(lambda x :checktargetInv(x,"ReferenceValue"),axis=1)
+leakage["wieghted_Target"]=leakage.apply(lambda x :checktargetInv(x,"TargetValue"),axis=1)
+leakage["wieghted_Reference"]=leakage.apply(lambda x :checktargetInv(x,"ReferenceValue"),axis=1)
+
+#leakage & delay grouping 
+leakage_sum=leakage.groupby(['PexCorner','PexTemp']).sum()
+leakage_sum['newChange']=(leakage_sum['wieghted_Target']-leakage_sum['wieghted_Reference'])*100/leakage_sum['wieghted_Reference']
+delay_sum=delay.groupby(['PexCorner','PexTemp']).sum()
+delay_sum['Ref_freq']=1/delay_sum['wieghted_Reference']
+delay_sum['targ_freq']=1/delay_sum['wieghted_Target']
+delay_sum['freq_change']=(delay_sum['targ_freq']-delay_sum['Ref_freq'])*100/delay_sum['Ref_freq']
+leakage_sum=leakage.groupby(['PexCorner','PexTemp']).sum()
+delay_sum['newChange']=(delay_sum['wieghted_Target']-delay_sum['wieghted_Reference'])*100/delay_sum['wieghted_Reference']
+delay_sum.drop(delay_sum.loc[:,'Track':'ReferenceValue'].columns,axis=1,inplace=True)
+delay_sum=delay_sum.transpose()
+leakage_sum['newChange']=(leakage_sum['wieghted_Target']-leakage_sum['wieghted_Reference'])*100/leakage_sum['wieghted_Reference']
+leakage_sum['newChange']=(leakage_sum['wieghted_Target']-leakage_sum['wieghted_Reference'])*100/leakage_sum['wieghted_Reference']
+leakage_sum=leakage_sum.transpose()
+
+#write file
+writer=pd.ExcelWriter(writeFile)
+leakage.to_excel(writer,sheet_name="leakage")
+delay.to_excel(writer,sheet_name="delay")
+leakage_sum.to_excel(writer,sheet_name="leakage_avg")
+delay_sum.to_excel(writer,sheet_name="Delay_avg")
+writer.save()
--- wv.py
+++ wv.py
@@ -1,7 +1,6 @@
 import pandas as pd
 df = pd.read_excel('delay.xlsx')
 column_names = df.columns
-print(column_names)
 stage_delay_df = df[(df['Parameter']=='stage_delay')]
 file_path = 'test.xlsx'
 stage_delay_df.to_excel(file_path, index=False)
@@ -19,32 +18,38 @@ def highlight_rows(row):
 
 sorted_df['target_wval']=None
 sorted_df['reference_wval']=None
+sorted_df['percentage_change']=None
 
 def write_data(filtered_df):
     #target weighted value calculation
     tar_val=filtered_df['TargetValue']
     tar_val_list=tar_val.to_list()
-    w_val=tar_val_list[0]*0.4
+    tar_w_val=tar_val_list[0]*0.4
     for val in tar_val_list[1:]:
-        w_val=w_val+0.1*val
-
+        tar_w_val=tar_w_val+0.1*val
+    tar_freq=1/tar_w_val
     row_numbers = filtered_df.index.to_list()
-    sorted_df.loc[row_numbers[0],'target_wval']=w_val
+    sorted_df.loc[row_numbers[0],'target_wval']=tar_w_val
     freq_loc=row_numbers[1]
-    sorted_df.loc[freq_loc,'target_wval']=1/w_val
+    sorted_df.loc[freq_loc,'target_wval']=tar_freq
 
     #reference weighted value calculation
     ref_val=filtered_df['ReferenceValue']
     ref_val_list=ref_val.to_list()
-    w_val=ref_val_list[0]*0.4
+    ref_w_val=ref_val_list[0]*0.4
     
     for val in ref_val_list[1:]:
-        w_val=w_val+0.1*val
+        ref_w_val=ref_w_val+0.1*val
 
-    row_numbers = filtered_df.index.to_list()
-    sorted_df.loc[row_numbers[0],'reference_wval']=w_val
-    freq_loc=row_numbers[1]
-    sorted_df.loc[freq_loc,'reference_wval']=1/w_val
+    ref_freq=1/ref_w_val
+    sorted_df.loc[row_numbers[0],'reference_wval']=ref_w_val
+    sorted_df.loc[freq_loc,'reference_wval']=ref_freq
+
+    per_delay_change=((tar_w_val-ref_w_val)/ref_w_val)*100
+    per_freq_change=((tar_freq-ref_freq)/ref_freq)*100
+
+    sorted_df.loc[row_numbers[0],'percentage_change']=per_delay_change
+    sorted_df.loc[freq_loc,'percentage_change']=per_freq_change
 
 
 
@@ -59,6 +64,14 @@ write_data(filtered_df)
 
 #for funcmin 125 FF
 filtered_df = sorted_df[(sorted_df['PexCorner'] =='FuncCmin') & (sorted_df['Temp'] == 125) & (sorted_df['ProcessCorner']=='FFG')]
+write_data(filtered_df)
+
+# #for FuncCmax -40 SS
+filtered_df = sorted_df[(sorted_df['PexCorner'] =='FuncCmax') & (sorted_df['Temp'] == -40) & (sorted_df['ProcessCorner']=='SSG')]
+write_data(filtered_df)
+
+# #for FuncCmax 125 SS
+filtered_df = sorted_df[(sorted_df['PexCorner'] =='FuncCmax') & (sorted_df['Temp'] == 125) & (sorted_df['ProcessCorner']=='SSG')]
 write_data(filtered_df)
 
 styled_df = sorted_df.style.apply(highlight_rows, axis=1)
