912c3998abea09d01949bd1e6e76a7945b33ab24
shawon-ulka <md.shawon.islam@ulkasemi.com>
2023-05-22 18:33:40 +0600

    added sheet name

--- wv.py
+++ wv.py
@@ -1,11 +1,7 @@
 import pandas as pd
 df = pd.read_excel('delay.xlsx')
 stage_delay_df = df[(df['Parameter']=='stage_delay')]
-# file_path = 'test.xlsx'
-# stage_delay_df.to_excel(file_path, index=False)
-
-
-# dfnew = pd.read_excel('test.xlsx')
+iddq_stage_df =  df[(df['Parameter']=='iddq_stage')]
 sorted_df = stage_delay_df.sort_values(by=['Temp','ProcessCorner'])
 
 
@@ -51,28 +47,22 @@ def write_data(filtered_df):
     sorted_df.loc[freq_loc,'percentage_change']=per_freq_change
 
 
-
-
 #for nominal tt corner
 filtered_df = sorted_df[(sorted_df['PexCorner'] =='Nominal') & (sorted_df['Temp'] == 25) & (sorted_df['ProcessCorner']=='TT')]
 write_data(filtered_df)
-
 #for funcmin -40 FF
 filtered_df = sorted_df[(sorted_df['PexCorner'] =='FuncCmin') & (sorted_df['Temp'] == -40) & (sorted_df['ProcessCorner']=='FFG')]
 write_data(filtered_df)
-
 #for funcmin 125 FF
 filtered_df = sorted_df[(sorted_df['PexCorner'] =='FuncCmin') & (sorted_df['Temp'] == 125) & (sorted_df['ProcessCorner']=='FFG')]
 write_data(filtered_df)
-
 # #for FuncCmax -40 SS
 filtered_df = sorted_df[(sorted_df['PexCorner'] =='FuncCmax') & (sorted_df['Temp'] == -40) & (sorted_df['ProcessCorner']=='SSG')]
 write_data(filtered_df)
-
 # #for FuncCmax 125 SS
 filtered_df = sorted_df[(sorted_df['PexCorner'] =='FuncCmax') & (sorted_df['Temp'] == 125) & (sorted_df['ProcessCorner']=='SSG')]
 write_data(filtered_df)
 
 styled_df = sorted_df.style.apply(highlight_rows, axis=1)
 file_path = 'output.xlsx'
-styled_df.to_excel(file_path, index=False)
+styled_df.to_excel(file_path,sheet_name="stage_delay", index=False)
